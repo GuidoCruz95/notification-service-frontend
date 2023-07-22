866029486412ba05dd82aec3d4bdc3767ee52bba
Guido Cruz <guido.cruz@ancient.mx>
2023-07-22 17:01:49 -0400

    Readme file updated

--- src/mock-files/category-mock-data.js
+++ src/mock-files/category-mock-data.js
@@ -2,7 +2,7 @@ export const categories = [
     {
         "id": "3f3b1fec-5982-4357-88f7-94bf76eb4266",
         "name": "Sports",
-        "description": "sports categorie"
+        "description": "sports category"
     },
     {
         "id": "123f3b7d-a183-4856-9aca-c3da1fdb2d76",
@@ -13,10 +13,5 @@ export const categories = [
         "id": "823b8060-ff0a-4128-a64e-dfb0d0914fcf",
         "name": "Movies",
         "description": "Movies category"
-    },
-    {
-        "id": "58ccf5d0-a1c8-4ab1-9863-1ffdfed34e9b",
-        "name": "someCategory",
-        "description": "Here a new category"
     }
 ]
